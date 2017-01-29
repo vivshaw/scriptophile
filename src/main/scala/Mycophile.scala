e0f83c0060deb8e16ebba84c3fb6112d85025c84
vivshaw <hvivianshaw@gmail.com>
2017-01-29 15:28:12 -0500

    Implement backprop and evaluation

--- src/main/scala/Mycophile.scala
+++ src/main/scala/Mycophile.scala
@@ -9,10 +9,28 @@ import breeze.numerics._
 import network.NeuralNetwork
 
 object Mycophile extends App {
-	println("Hello, Scala!")
-	var net = NeuralNetwork(List(20, 8, 5))
-	val test_matrix = DenseMatrix.zeros[Double](20,1)
-	val data = List((test_matrix, 1))
-	println(net.sgd(data, 10, 1, 0.5))
-	println(net.test)
+	var net = NeuralNetwork(List(2, 2, 2))
+	val zero = new DenseMatrix(2,1,DenseVector(0.0, 1.0).toArray)
+	val one = new DenseMatrix(2,1,DenseVector(1.0, 0.0).toArray)
+	val train_data = List((DenseMatrix.rand(2, 1, breeze.stats.distributions.Gaussian(0, 1)), zero), (DenseMatrix.rand(2, 1, breeze.stats.distributions.Gaussian(0, 1)), one))
+
+	println("pre-train evaluation: ")
+	println(net.evaluate(train_data) + "/" + train_data.length + " correct")
+
+	println("now training")
+	net.sgd(train_data, 50000, 1, 0.1)
+
+	println("evaluation after training")
+	println(net.evaluate(train_data) + "/" + train_data.length + " correct")
+
+	println("datum 1")
+	println(net.feedForward(train_data(0)._1))
+	println(argmax(net.feedForward(train_data(0)._1)))
+
+	println("datum 2")
+	println(net.feedForward(train_data(1)._1))
+	println(argmax(net.feedForward(train_data(1)._1)))
+
+	println("post-train evaluation: ")
+	println(net.evaluate(train_data) + "/" + train_data.length + " correct")
 }
--- src/main/scala/network/NeuralNetwork.scala
+++ src/main/scala/network/NeuralNetwork.scala
@@ -11,44 +11,85 @@ import scala.util.Random.shuffle
 class NeuralNetwork(sizes: List[Int]) {
 	val layers = sizes.length
 	val normal = breeze.stats.distributions.Gaussian(0, 1)
-	var biases = for (y <- sizes.drop(1) ) yield DenseMatrix.rand(y, 1, normal)
+	var biases = for (y <- sizes.drop(1)) yield DenseMatrix.rand(y, 1, normal)
 	var weights = for (t <- sizes.dropRight(1) zip sizes.drop(1)) yield DenseMatrix.rand(t._2, t._1, normal)
-	var test: Double = 0
 
 	def feedForward (activation: DenseMatrix[Double]) : DenseMatrix[Double] = {
 		var output = activation
-		biases.zip(weights).foreach{ case (bias, weight) => 
+
+		biases zip weights foreach { case (bias, weight) => 
 			output = sigmoid((weight * output) + bias)
 		}
+
 		return output
 	}
 
-	def sgd (trainingData: List[Tuple2[DenseMatrix[Double], Int]], epochs: Int, miniBatchSize: Int, eta: Double) {
+	def sgd (trainingData: List[Tuple2[DenseMatrix[Double], DenseMatrix[Double]]], epochs: Int, miniBatchSize: Int, eta: Double) {
 		val n = trainingData.length
-		for(i <- 1 to epochs) {
+
+		for (i <- 1 to epochs) {
 			val data = shuffle(trainingData)
-			val miniBatches = for (j <- 0 to n by miniBatchSize) yield trainingData.slice(j, j + miniBatchSize)
-			miniBatches.foreach{ miniBatch => updateMiniBatch(miniBatch, eta) }
+			val miniBatches = for (j <- 0 to n - 1 by miniBatchSize) yield trainingData.slice(j, j + miniBatchSize)
+
+			miniBatches foreach { miniBatch => 
+				updateMiniBatch(miniBatch, eta)
+			}
+
 			println("Epoch " + i + " complete.")
 		}
 	}
 
-	def updateMiniBatch (miniBatch: List[Tuple2[DenseMatrix[Double], Int]], eta: Double) {
+	def updateMiniBatch (miniBatch: List[Tuple2[DenseMatrix[Double], DenseMatrix[Double]]], eta: Double) {
 		var nabla_bias = for (bias <- biases) yield DenseMatrix.zeros[Double](bias.rows, bias.cols)
 		var nabla_weight = for (weight <- weights) yield DenseMatrix.zeros[Double](weight.rows, weight.cols)
-		miniBatch.foreach{ case (features, result) =>
+
+		miniBatch foreach { case (features, result) =>
 			val (delta_nabla_bias, delta_nabla_weight) = backprop(features, result)
 			nabla_bias = for (t <- nabla_bias zip delta_nabla_bias) yield t._1 + t._2
 			nabla_weight = for (t <- nabla_weight zip delta_nabla_weight) yield t._1 + t._2	
 		}
-		weights = for(t <- weights zip nabla_weight) yield t._1 - (t._2 * (eta / miniBatch.length))
-		biases = for(t <- biases zip nabla_bias) yield t._1 - (t._2 * (eta / miniBatch.length))
+
+		weights = for (t <- weights zip nabla_weight) yield t._1 - (t._2 * (eta / miniBatch.length))
+		biases = for (t <- biases zip nabla_bias) yield t._1 - (t._2 * (eta / miniBatch.length))
 	}
 
-	def backprop (features: DenseMatrix[Double], result: Int) : (List[DenseMatrix[Double]], List[DenseMatrix[Double]]) = {
+	def backprop (features: DenseMatrix[Double], result: DenseMatrix[Double]) : (List[DenseMatrix[Double]], List[DenseMatrix[Double]]) = {
 		var nabla_bias = for (bias <- biases) yield DenseMatrix.zeros[Double](bias.rows, bias.cols)
 		var nabla_weight = for (weight <- weights) yield DenseMatrix.zeros[Double](weight.rows, weight.cols)
+		
+		var activation = features
+		var activations = List(features)
+		var zs: List[DenseMatrix[Double]] = List()
+
+		biases zip weights foreach { case (bias, weight) => 
+			val z = weight * activation
+			zs = zs :+ z
+			activation = sigmoid(z)
+			activations = activations :+ activation
+		}
+
+		var delta = (activations.reverse.head - result) :* sigmoid_prime(zs.reverse.head)
+		nabla_bias = nabla_bias.updated(nabla_bias.length - 1, delta)
+		nabla_weight = nabla_weight.updated(nabla_weight.length - 1, delta * activations.takeRight(2).head.t)
+
+		for (i <- 2 to layers - 1) {
+			val z = zs.takeRight(i).head
+			val sp = sigmoid_prime(z)
+			delta = (weights.takeRight(i - 1).head.t * delta) :* sp
+			nabla_bias = nabla_bias.updated(nabla_bias.length - 1 - i, delta)
+			nabla_weight = nabla_weight.updated(nabla_weight.length - 1 - i, delta * activations.takeRight(i + 1).head.t)
+		}
+
 		return (nabla_bias, nabla_weight)
+	}
+
+	def evaluate (test_data: List[Tuple2[DenseMatrix[Double], DenseMatrix[Double]]]) : Int = {
+		val correct = for (t <- test_data if argmax(feedForward(t._1)) == argmax(t._2)) yield 1
+		return correct.length
+	}
+
+	def sigmoid_prime (z: DenseMatrix[Double]) : DenseMatrix[Double] = {
+		return sigmoid(z) :* (-sigmoid(z) + 1d)
 	}
 }
 

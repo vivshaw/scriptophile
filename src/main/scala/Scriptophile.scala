7c743e875a1228ffc37d3391fc36786208f3e9bd
vivshaw <hvivianshaw@gmail.com>
2017-01-29 18:57:28 -0500

    Start reading csv & data cleaning

--- src/main/scala/Scriptophile.scala
+++ src/main/scala/Scriptophile.scala
@@ -16,7 +16,17 @@ object Mycophile extends App {
 	val excl_or = List((zero, one), (one, one), (both, zero), (neither, zero))
 	val incl_or = List((zero, one), (one, one), (both, one), (neither, zero))
 	val train_data = excl_or
+	
+	val bufferedSource = io.Source.fromFile("src/main/resources/mnist_train.csv")
+    for (line <- bufferedSource.getLines) {
+        val cols = line.split(",").map(_.trim)
+        // do whatever you want with the columns here
+        println(s"${cols(0)}")
+    }
+    bufferedSource.close
 
+	/*
 	println("now training")
 	net.sgd(train_data, 10000, 4, 0.5, train_data)
+	*/
 }

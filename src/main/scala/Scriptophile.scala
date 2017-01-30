35784bdea6898e2a3d1b969475f032b447ee5504
vivshaw <hvivianshaw@gmail.com>
2017-01-30 03:52:05 -0500

    Refactor case class & explain everything with comments

--- src/main/scala/Scriptophile.scala
+++ src/main/scala/Scriptophile.scala
@@ -1,40 +1,58 @@
 package scriptophile
 
 import scala.io.Source
-
 import breeze.linalg._
 import breeze.numerics._
 
 import network.NeuralNetwork
 
 object Scriptophile extends App {	
-    def digitToArray (digit: Int) : DenseMatrix[Double] = {
+    def labelToArray (digit: Int) : DenseMatrix[Double] = {
+        /* Turns a digit into a (10,1) matrix representing the unit vector for that digit
+        *  These are used to represent the label from the MNIST data.
+        */
     	val vect = DenseVector.zeros[Double](10)
     	vect(digit) = 1.0
     	var digitMatrix = new DenseMatrix(10, 1, vect.toArray)
     	return digitMatrix
     }
 
-    def pixelToArray (pixel: Array[Double]) : DenseMatrix[Double] = {
-    	val out = new DenseMatrix(784, 1, pixel)
+    def imageToArray (image: Array[Double]) : DenseMatrix[Double] = {
+        /* Turns an array of doubles into a (784,1) matrix representing the 784 pixels of an image
+        *  from the MNIST data.
+        */
+    	val out = new DenseMatrix(784, 1, image)
     	return out
     }
 
 	case class mnistDatum(line: String) {
+        /* Case class used to process the raw CSV data into an Array[Double] containing the image,
+        *  and an Int label. Also performs feature rescaling on the pixels to [0, 1].
+        */
 		val raw = line.split(",").map(_.trim)
-		val data = (pixelToArray(raw.tail map(item => item.toDouble / 255.0)),
-					digitToArray(raw.head.toInt))
+        val label = labelToArray(raw.head.toInt)
+        val image = imageToArray(raw.tail map(item => item.toDouble / 255.0))
 	}
 	
+    /* Load MNIST data from a 785-column CSV where column 0 is the label (a digit from 0 to 9), and columns 1...784
+    *  each represent one pixel from the 28x28 MNIST image. These both get transformed into (n,1) matrices, then put
+    *  into a List[Tuple2[]] where the first element of each tuple is an image, and the second element is its label.
+    */
     val mnistRaw = Source.fromFile("src/main/resources/mnist_train.csv") getLines() drop(1) map(line => mnistDatum(line))
-    val mnistData = mnistRaw map(datum => datum.data)
+    val mnistData = mnistRaw map(datum => (datum.image, datum.label))
     val mnist = mnistData.toSeq
 
+    // Test/train split
     val mnist_train = mnist.dropRight(10000)
     val mnist_test = mnist.takeRight(10000)
     
+    /* Initialize 3-layer neural network with 784 input neurons corresponding to te pixel of a MNIST image, a 30-neuron hidden layer,
+    *  and 10 output neurons corresponding to the 10 digit labels.
+    */
+    val net = NeuralNetwork(List(784, 30, 10))
+
 	println("now training")
-	val net = NeuralNetwork(List(784, 30, 10))
+    // Train for 30 epochs, with mini-batch size 10, and learning rate 3.0
 	net.sgd(mnist_train, 30, 10, 3.0, mnist_test)
 
 	println(s"final accuracy: ${ net.evaluate(mnist_test).toDouble / mnist_test.length.toDouble }%")
--- src/main/scala/network/NeuralNetwork.scala
+++ src/main/scala/network/NeuralNetwork.scala
@@ -1,16 +1,27 @@
+/* NeuralNetwork.scala
+*  
+*  A class implementing a simple neural network and training it with mini batch stochastic gradient descent via
+*  backpropagation. Inspired by Michael Nielsen's Python implementation in Neural Networks and Deep Learning http://neuralnetworksanddeeplearning.com/
+*/
+
 package network
 
+import scala.util.Random.shuffle
 import breeze.linalg._
 import breeze.numerics._
-import scala.util.Random.shuffle
 
 class NeuralNetwork(sizes: Seq[Int]) {
+	/* sizes contains the size of each layer of neurons in the network. So, if sizes were Seq(4, 3, 3, 2), the network would
+	*  be a 4-layer network with 4 input neurons, two hidden layers of 3 neurons each, and an output layer with 2 neurons.
+	*/
 	val layers = sizes.length
 	val normal = breeze.stats.distributions.Gaussian(0, 1)
 	var biases = for (y <- sizes.drop(1)) yield DenseMatrix.rand(y, 1, normal)
 	var weights = for (t <- sizes.dropRight(1) zip sizes.drop(1)) yield DenseMatrix.rand(t._2, t._1, normal)
 
 	def feedForward (activation: DenseMatrix[Double]) : DenseMatrix[Double] = {
+		/* Plug an activation into the network and return the output
+		*/
 		var output = activation
 
 		biases zip weights foreach { case (bias, weight) => 
@@ -21,6 +32,10 @@ class NeuralNetwork(sizes: Seq[Int]) {
 	}
 
 	def sgd (trainingData: Seq[Tuple2[DenseMatrix[Double], DenseMatrix[Double]]], epochs: Int, miniBatchSize: Int, eta: Double, testData: Seq[Tuple2[DenseMatrix[Double], DenseMatrix[Double]]]) {
+		/* Perform mini batch stochastic gradient descent to train the network, outputting the test accuracy at each epoch. The training
+		*  and test data are both Seq[Tuple2[]] of DenseMatrix[Doubles], where each tuple is an input / target output pair, and the rest
+		*  of the arguments do what they say on the tin.
+		*/
 		val n = trainingData.length
 
 		for (i <- 1 to epochs) {
@@ -36,6 +51,10 @@ class NeuralNetwork(sizes: Seq[Int]) {
 	}
 
 	def updateMiniBatch (miniBatch: Seq[Tuple2[DenseMatrix[Double], DenseMatrix[Double]]], eta: Double) {
+		/* Updates weights and biases via backpropagation over one minibatch. miniBatch is a Seq[Tuple2[]]
+		*  of DenseMatrix[Double]s where each Tuple2 is an input / target output pair, and eta
+		*  is the learning rate.
+		*/
 		var nabla_bias = for (bias <- biases) yield DenseMatrix.zeros[Double](bias.rows, bias.cols)
 		var nabla_weight = for (weight <- weights) yield DenseMatrix.zeros[Double](weight.rows, weight.cols)
 
@@ -50,11 +69,12 @@ class NeuralNetwork(sizes: Seq[Int]) {
 	}
 
 	def backprop (features: DenseMatrix[Double], result: DenseMatrix[Double]) : (Seq[DenseMatrix[Double]], Seq[DenseMatrix[Double]]) = {
-		// Initialize nabla_bias, nabla_weight to zeros matrix with correct dimension
+		/* Returns the gradient of the cost function as a Tuple2[] of DenseMatrix[Double]s, where nabla_bias
+		*  and nabla_weight are both Seq[DenseMatrix[Double]] just like weights and biases
+		*/
 		var nabla_bias = for (bias <- biases) yield DenseMatrix.zeros[Double](bias.rows, bias.cols)
 		var nabla_weight = for (weight <- weights) yield DenseMatrix.zeros[Double](weight.rows, weight.cols)
 		
-
 		// feedforward pass, storing z values
 		var activation = features
 		var activations = List(features)
@@ -79,10 +99,14 @@ class NeuralNetwork(sizes: Seq[Int]) {
 			nabla_bias = nabla_bias.updated(nabla_bias.length - i, delta)
 			nabla_weight = nabla_weight.updated(nabla_weight.length - i, delta * activations.takeRight(i + 1).head.t)
 		}
+
 		return (nabla_bias, nabla_weight)
 	}
 
 	def evaluate (test_data: Seq[Tuple2[DenseMatrix[Double], DenseMatrix[Double]]]) : Int = {
+		/* Returns the number of inputs from test_data for which the network's response is correct.
+		*  The output is calculated as the index of the output neuron with the maximum activation.
+		*/
 		val correct = for (t <- test_data if argmax(feedForward(t._1)) == argmax(t._2)) yield 1
 		return correct.length
 	}
